532e1bc82af3fad52dad015e3971919f384a445a
JB <joshbrown2794@gmail.com>
2021-01-13 17:46:15 -0800

    footer updatded with links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../Styles/Footer.css";
+
 function Footer() {
   return (
     <div>
       <Navbar fixed="bottom" expand="lg">
         <Container>
           <div className="footer__container">
-            <h1>hello world from the footer</h1>
+            <div className="icon__container">
+              <a href="https://github.com/natewinter">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+              <a href="https://www.linkedin.com/in/josh-brown-a874421bb"></a>
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </div>
           </div>
         </Container>
       </Navbar>
